e3b328b7f975d8a0ae991b212fc16ad047c34e00
Abidalfakhri <abidfakhri5@gmail.com>
2025-10-25 19:59:38 +0700

    2

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
-import Navbar from './components/layout/Navbar';
-import Sidebar from './components/layout/Sidebar';
-import Footer from './components/layout/Footer';
+import Navbar from '@/components/layout/Navbar';
+import Sidebar from '@/components/layout/Sidebar';
+import Footer from '@/components/layout/Footer';
 import { useAuth } from '../contexts/AuthContext';
 
 export default function DashboardLayout() {
